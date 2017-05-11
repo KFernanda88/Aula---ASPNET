1066c41aa5849519d54b03f7e5fad5f256c13914
KFernanda88 <k.fer88@gmail.com>
2017-05-11 20:16:54 -0300

    Criando projeto aula 11/05

---
+++ Aula 1105/Aula 1105/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aula_1105.Startup))]
+namespace Aula_1105
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
